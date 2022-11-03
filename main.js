04082817465f65e41d8373ba34230f0e4e74aec0
Ezknow <ljo722@naver.com>
2022-11-03 21:06:46 +0900

    JS : navbar menu toggle + project UI

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ document.addEventListener('scroll', () => {
 })
 
 
+
 // Handle scrolling when tapping on the navbar menu
 
 const navbarMenu = document.querySelector('.navbar__menu');
@@ -24,8 +25,17 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove('open')
     console.log(event.target.dataset.link);
     scrollIntoView(link)
+})
+
+
+// Navbar toggle button for small screen
+
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open')
 })
 
 
